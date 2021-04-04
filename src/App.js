94cde28dc3c2e9b10705796d724754db723ae82b
Amory <garbage90@yandex.ru>
2021-04-04 22:07:07 +0300

    section mission

--- src/App.js
+++ src/App.js
@@ -73,6 +73,13 @@ const useStyles = makeStyles((theme) => ({
   cardContent: {
     flexGrow: 1,
   },
+  cardHor: {
+    paddingTop: '50%',
+  },
+  cardVert: {
+    paddingTop: '100%',
+  },
+
 }))
 
 function App() {
@@ -108,7 +115,7 @@ function App() {
       </AppBar>
 
       <main>
-        <Paper fixed className={classes.mainImg} style={{backgroundImage: `url(https://source.unsplash.com/random)`}}>
+        <Paper  className={classes.mainImg} style={{backgroundImage: `url(https://source.unsplash.com/random)`}}>
           <Container maxWidth="md">
           <div className={classes.overlay}/>
             <Grid container>
@@ -142,7 +149,7 @@ function App() {
         </section>
 
         <section>
-          <Container className={classes.cardGrid} maxWidth="md">
+          <Container className={classes.cardGrid} maxWidth="md" >
             <Grid container spacing={4}>
               {cards.map((card) => (
                 <Grid item key={card} xs={12} sm={4} md={3}>
@@ -153,10 +160,10 @@ function App() {
                       title="Image title"
                     />
                     <CardContent className={classes.cardContent}>
-                      <Link gutterBottom variant="h7" component="p" color="inherit">
+                      <Link gutterBottom variant="subtitle1" component="p" color="inherit">
                         Preorder - Analog Wood Card Holder
                       </Link>
-                      <Typography  variant="h8" component="p">
+                      <Typography  variant="subtitle2" component="p">
                         $59.00
                       </Typography>
                     </CardContent>
@@ -165,6 +172,63 @@ function App() {
               ))}
             </Grid>
         </Container>
+        </section>
+
+        <section>
+          <Container maxWidth="md" >
+            <Grid container maxWidth="sm" justify="center" spacing={4}>
+              <Grid item md={6}>               
+                  <Typography component="p" variant="h5" align="center">
+                    Our mission is to create simple, beautiful objects that combine form and function.
+                  </Typography>
+              </Grid>
+            </Grid>
+              
+            <Grid container className={classes.cardGrid} spacing={10} >
+              <Grid item xs={6}>
+                <Card variant="elevation" elevation={0}>
+                  <CardMedia
+                    className={classes.cardVert}
+                    image="https://source.unsplash.com/random"
+                    title="Image title"
+                  />
+                  <CardContent className={classes.cardContent}>
+                    <Link gutterBottom variant="subtitle1" component="p" color="inherit">
+                      Shop Mens →
+                    </Link>
+                  </CardContent>
+                </Card>
+              </Grid>
+              <Grid item xs={6}>
+                <Card variant="elevation" elevation={0}>
+                  <CardMedia
+                    className={classes.cardVert}
+                    image="https://source.unsplash.com/random"
+                    title="Image title"
+                  />
+                  <CardContent className={classes.cardContent}>
+                    <Link gutterBottom variant="subtitle1" component="p" color="inherit">
+                      Shop Womens →
+                    </Link>
+                  </CardContent>
+                </Card>
+              </Grid>
+              <Grid item xs={12}>
+                <Card variant="elevation" elevation={0}>
+                  <CardMedia
+                    className={classes.cardHor}
+                    image="https://source.unsplash.com/random"
+                    title="Image title"
+                  />
+                  <CardContent className={classes.cardContent}>
+                    <Link gutterBottom variant="subtitle1" component="p" color="inherit">
+                      Shop Objects →
+                    </Link>
+                  </CardContent>
+                </Card>
+              </Grid>
+            </Grid>
+          </Container>
         </section>
 
       </main>
